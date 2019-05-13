71d85194b7ba1d450097c7eb804d6bfa9b3fa3a9
Tyler Riddle <cardboardaardvark@gmail.com>
2019-05-12 22:03:21 -0700

    better assert()

--- pulsar/audio.cxx
+++ pulsar/audio.cxx
@@ -285,7 +285,7 @@ std::shared_ptr<audio::buffer> audio::input::get_buffer()
     } else if (num_links == 1) {
         log_trace("returning pointer to link's ready buffer for ", input_name);
         auto lock = pulsar_get_lock(link_buffers_mutex);
-        assert(link_buffers.begin() != link_buffers.end());
+        assert(link_buffers.size() == 1);
         assert(link_buffers.begin()->second != nullptr);
         return link_buffers.begin()->second;
     } else {
