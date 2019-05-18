cd74d31cb58c4bd04154f52fb8ec0e4bda6e41da
Tyler Riddle <cardboardaardvark@gmail.com>
2019-05-17 23:45:07 -0700

    add alternate DL location for boost

--- cmake/LocalBoost.cmake
+++ cmake/LocalBoost.cmake
@@ -11,6 +11,7 @@ ExternalProject_Add(
     BUILD_IN_SOURCE TRUE
 
     URL https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
+    URL https://newcontinuum.dl.sourceforge.net/project/boost/boost/1.66.0/boost_1_66_0.tar.gz
     URL_HASH SHA512=8d537fa1fcd24dfa3bc107741d20d93624851b724883c5cfe0cdcd8b8390939869e0f41a36f52d3051f129a8405a574a1a095897f82ece747740b34a1e52ffdb
 
     CONFIGURE_COMMAND ./bootstrap.sh
