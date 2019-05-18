837b426263085ea2f4fec35dee98645741633e48
Tyler Riddle <cardboardaardvark@gmail.com>
2019-05-17 20:57:28 -0700

    improve yaml-cpp builds

--- cmake/Localyaml-cpp.cmake
+++ cmake/Localyaml-cpp.cmake
@@ -1,13 +1,8 @@
 if (NOT DOWNLOAD_DEPS)
-    message(SEND_ERROR "Could not find a system YAML-cpp and DOWNLOAD_DEPS is OFF")
+    message(SEND_ERROR "Could not find a system yaml-cpp and DOWNLOAD_DEPS is OFF")
 endif (NOT DOWNLOAD_DEPS)
 
-message("  Will download and compile YAML-cpp")
-
-set(BUILD_SHARED_LIBS OFF)
-set(YAML_CPP_BUILD_TESTS OFF)
-set(YAML_CPP_BUILD_TOOLS OFF)
-set(YAML_CPP_INSTALL OFF)
+message("  Will download and compile yaml-cpp")
 
 ExternalProject_Add(
     yaml-cpp
@@ -16,9 +11,11 @@ ExternalProject_Add(
 
     URL https://github.com/jbeder/yaml-cpp/archive/yaml-cpp-0.6.2.tar.gz
     URL_HASH SHA512=fea8ce0a20a00cbc75023d1db442edfcd32d0ac57a3c41b32ec8d56f87cc1d85d7dd7a923ce662f5d3a315f91a736d6be0d649997acd190915c1d68cc93795e4
+    CMAKE_ARGS -DBUILD_SHARED_LIBS=OFF YAML_CPP_BUILD_TESTS=OFF YAML_CPP_BUILD_TOOLS=OFF YAML_CPP_INSTALL=OFF
+    TEST_EXCLUDE_FROM_MAIN ON
     INSTALL_COMMAND ""
 )
 
 set(LOCAL_YAML_CPP ON)
-set(YAML_CPP_INCLUDE_DIR "${CMAKE_BINARY_DIR}/yaml-cpp-prefix/src/yaml-cpp/")
+set(YAML_CPP_INCLUDE_DIR "${CMAKE_BINARY_DIR}/yaml-cpp-prefix/src/yaml-cpp/include")
 set(YAML_CPP_LIBRARIES "${CMAKE_BINARY_DIR}/yaml-cpp-prefix/src/yaml-cpp-build/libyaml-cpp.a")
